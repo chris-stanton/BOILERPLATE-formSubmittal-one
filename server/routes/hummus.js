52a6d5d77a1df6f2e317bc0bffdb1622b788d072
Chris Stanton <cstanton0760@yahoo.com>
2018-01-29 18:01:33 -0600

    added multipage pdf minipulation

--- server/routes/hummus.js
+++ server/routes/hummus.js
@@ -10,17 +10,17 @@ const hummus = require('hummus');
 router.post('/sendEmail', function(req, res){
   let form = req.body;
 
+    // defining file paths
 		var pdfWriter = hummus.createWriterToModify(__dirname + '/vet.pdf', {
 			modifiedFilePath: __dirname + '/pdf/' + form.firstName + form.lastName + '.pdf',
       log: './server/logs/hummus.logs.js'
 		});
 
-		var pageModifier = new hummus.PDFPageModifier(pdfWriter,0);
-
-
-    pageModifier.startContext().getContext().writeText(
-			'TEST 1',
-			30, 80,
+    // creates text on first page
+		var pageModifier0 = new hummus.PDFPageModifier(pdfWriter,0);
+    pageModifier0.startContext().getContext().writeText(
+			form.firstName + ' ' + form.lastName, // text
+			130, 580, // text position
 			{
         font:pdfWriter.getFontForFile('./server/routes/arial.ttf'),
         size:12,
@@ -28,10 +28,24 @@ router.post('/sendEmail', function(req, res){
         colorspace:'grey'
       }
 		);
+    pageModifier0.endContext().writePage();
 
-    
-		pageModifier.endContext().writePage();
-		pdfWriter.end();
+    // creates text on second page
+    var pageModifier1 = new hummus.PDFPageModifier(pdfWriter,1);
+    pageModifier1.startContext().getContext().writeText(
+			form.firstName + ' ' + form.lastName, // text
+			330, 80, // text position
+			{
+        font:pdfWriter.getFontForFile('./server/routes/arial.ttf'),
+        size:12,
+        color:'red',
+        colorspace:'grey'
+      }
+		);
+		pageModifier1.endContext().writePage();
+
+    // prints page
+    pdfWriter.end();
 
 
 
